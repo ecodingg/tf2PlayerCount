5f540ce3b2d24879e8982fbd0cda640dad7ec41a
Eden <ecoding@pm.me>
2024-05-18 20:21:45 -0700

    Started making the data, found the average

--- TF2DataDepiction.cs
+++ TF2DataDepiction.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Linq;
+using System.IO;
 
-public class TF2DataDepiction
+class TF2DataDepiction
 {
-	public TF2DataDepiction()
+    static void Main(string[] args)
 	{
-		public static void main()
+		Console.WriteLine("This is the TF2 Data presented by ZestyJesus & Mega Scatter Bomb using statistics");
+
+		List<int> playerNumbers = new List<int>();
+		string filePath = "C:\\Users\\rexer\\Documents\\GitHub\\tf2PlayerCount\\steam_api_data.csv";
+
+		Console.WriteLine("Adding all of the data to a List");
+		using (var reader = new StreamReader(filePath))
 		{
-			Console.WriteLine("Hello World!");
+			while(!reader.EndOfStream)
+			{
+				var line = reader.ReadLine();
+				var values = line.Split(',');
+
+                foreach (var value in values)
+				{
+					playerNumbers.Add(int.Parse(values[1]));
+				}
+			}
+			Console.WriteLine("Average Number of Players: " + playerNumbers.Average());
 		}
-	}
+
+    }
 }
